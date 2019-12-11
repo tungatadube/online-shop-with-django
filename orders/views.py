9a13e3e54e11952a510725ec2b75e5ac84a2d39c
Mduduzi <mduduzi.dube@cassavasmartech.co.zw>
2019-12-11 10:38:33 +0200

    Created a form for placing orders, a view for prcessing the form, registerd the form on the admin site and created views for the form landing page as well as the success page after posting

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from cart.cart import Cart
+from orders.forms import OrderCreateForm
+from orders.models import OrderItem
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity']
+                )
+
+            # clear the cart
+            cart.clear()
+            return render(request, '/order/created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+        return render(request, 'order/create.html', {'cart': cart, 'form': form})
