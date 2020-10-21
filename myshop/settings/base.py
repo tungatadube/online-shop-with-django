df98210965f8a7e9762e14304eeac2b435b0cad3
Mduduzi Dube <mduduzi.dube@cassavasmartech.co.zw>
2020-10-21 08:30:17 +0200

    Added initial migrations and created staticdirs path

--- myshop/settings/base.py
+++ myshop/settings/base.py
@@ -131,6 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ["/home/frederick/Documents/practice/onlineShop/online-shop-with-django/myshop/static/css/shop"]
 
 # Storing a cart in the user session
 CART_SESSION_ID = 'cart'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
